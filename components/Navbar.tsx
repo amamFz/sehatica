2ae1e3bb87ceea50a2731e2a853cd49be942726c
farrel irawan <irawanssfarrel@gmail.com>
2025-03-03 19:43:23 +0700

    menambahkan komponen label dan navigation menu dari shadcn

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <header className="py-3 px-5 flex items-center bg-secondary">
+            <div className="container w-full mx-auto">
+                
+            </div>
+        </header>
+    )
+}
+
+export default Navbar;
