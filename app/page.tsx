0670136af8748bab31ac0c021e8edf4852a9dc8f
farrel irawan <irawanssfarrel@gmail.com>
2025-03-06 22:04:25 +0700

    menambahkan gambar contoh artikel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Homepage from "@/components/sections/Homesection/Homepage";
+import Categorysection from "@/components/sections/Categorysection/Categorysection";
 
 export default function Home() {
   return (
     <>
     <Homepage />
+    <Categorysection />
     </>
   );
 }
