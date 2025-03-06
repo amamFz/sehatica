4ba610ddaae1d7ddf2d9415950d79baecd75987f
farrel irawan <irawanssfarrel@gmail.com>
2025-03-06 21:55:13 +0700

    membuat card article

---
+++ components/CardArticles/CardArticles.tsx
@@ -0,0 +1,33 @@
+import { 
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../ui/card";
+import { Button } from "../ui/button";
+import { ArticleProps } from "@/types/type";
+
+interface CardDataProps {
+    data: ArticleProps;
+}
+
+export default function CardArticles({ data }: CardDataProps) {
+    return(
+    <Card className="flex flex-col justify-between">
+            <CardHeader className="w-full">
+                <img src={data.image} alt="photo-1" className="w-[300px] h-full object-cover"/>
+            </CardHeader>
+            <CardContent>
+                <div className="flex flex-col gap-2">
+                    <CardTitle className="w-fit text-xl text-blue-500">{data.title}</CardTitle>
+                    <CardDescription className="w-fit text-base">{data.description}</CardDescription>
+                </div>
+            </CardContent>
+            <CardFooter>
+                <Button className="w-full bg-blue-500 transition-all duration-300 ease-out hover:bg-blue-600">Read More</Button>
+            </CardFooter>
+    </Card>
+    )
+}
---
+++ types/type.ts
@@ -0,0 +1,5 @@
+export type ArticleProps = {
+    title: string;
+    description: string;
+    image: string;
+}
