83d5d9baafaef6e570551b81ca1515de8c61fba9
farrel irawan <irawanssfarrel@gmail.com>
2025-03-06 23:45:43 +0700

    menyelesaikan homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import Homesection from "@/components/sections/Homesection/Homesection";
 import Categorysection from "@/components/sections/Categorysection/Categorysection";
 import Featuredsection from "@/components/sections/Featuredsection/Featuredsection";
 import Shopsection from "@/components/sections/Shopsection/Shopsection";
+import Comunitysection from "@/components/sections/Comunitysection/Comunitysection";
 
 export default function Home() {
   return (
     <>
-      <Homesection />
-      <Categorysection />
-      <Featuredsection />
-      <Shopsection />
+      <main>
+        <Homesection />
+        <Categorysection />
+        <Featuredsection />
+        <Shopsection />
+        <Comunitysection />
+      </main>
     </>
   );
 }
